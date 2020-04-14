493fec137753094294d7cdaae84af1d3dc36fba7
GlennWasAlreadyTaken <glenn.pedron@insa-rennes.fr>
2020-04-14 18:25:16 +0200

    Added some comments

--- gym_dkagent/envs/dk_env.py
+++ gym_dkagent/envs/dk_env.py
@@ -1,32 +1,68 @@
 # -*- coding: utf-8 -*-
 import socket
 import gym
-
+import threading
 
 class DKEnv(gym.Env):
     """
     DonkeyKong Environment.
+    
+    Observation: 
+        * Background of the level
+        * Objects in the level
+        
+    Actions:
+        Type: Discrete(2)
+        Num	Action
+        0	Left
+        1	Right
+        2   Up
+        3   Down
+        4   A Button
+        5   B Button
+
+    Reward:
+        ?
+
+    Episode Termination:
+        Level finished
     """
     
-    def reset():
+    def __init__(self):
+        pass
+    
+    def reset(self):
         observation = 0
         return observation
     
-    def step(action):
+    def step(self, action):
         observation, reward, done, info = 0
         
         return observation, reward, done, info
     
-    def render(mode='human'):
+    def render(self, mode='human'):
         pass
 
-
-
-class Network:
-    def __init__(self, host = '127.0.0.1', port = 36297):
+"""
+    Class responsible of creating a server for the communication
+    between Bizhawk and the DK environnement.
+"""
+class DKServer(threading.Thread):
+    
+    def __init__(self, port = 36297, num_env=-1):
+        super().__init__()
+        
+        self.port = port + num_env
+        
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((host, port))
-        self.sock.listen(1)
+        self.sock.bind(('localhost', self.port))
+
+    # Executed when the thread is started.
+    def run(self):
+        # We wait until only 1 connection is made.
+        self.sock.listen(1) 
+        
+        # We accept the connection and 
         self.connection, self.address = self.sock.accept()
         print('Network got a client at {}'.format(self.address))
 
@@ -39,7 +75,8 @@ class Network:
 
     def close(self):
         _ = self.connection.close()
-        
+
+"""
 net = Network()
 msg = ""
 while msg != "exit":
@@ -49,3 +86,4 @@ while msg != "exit":
     print(msg)
     
 net.close()
+"""
