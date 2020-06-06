39404662823a306425f52e05d45f3cd39f4a6a8c
GlennWasAlreadyTaken <glenn.pedron@insa-rennes.fr>
2020-06-06 15:01:57 +0200

    Corrected a bug on done state on lua script

--- gym_dkagent/DonkeyKong.lua
+++ gym_dkagent/DonkeyKong.lua
@@ -257,8 +257,8 @@ while true do
 		if memory.read_u8(0x0096) == 255 then 	-- if mario dies
 			done = 1 
 			reward = -1000
-		elseif memory.read_u8(0x0053) ~= 1 then		-- if mario wins (the adress contains the level number) 
-			done = 0
+		elseif memory.read_u8(0x0053) == 3 then		-- if mario wins (the adress contains the level number) 
+			done = 1
 			reward = 1000
 		end
 
